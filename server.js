cb28c3684131f228f949b320b1f5142f3f221e22
tanisha2398 <tanishanegipro@gmail.com>
2019-05-31 14:32:18 +0530

    add express library and setting port on 3000 and handler for 2 route-/,about,bad

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+var app = express();
+
+app.get("/", (req, res) => {
+  //   res.send("<h1>Hello express!</h1>");
+
+  res.send({
+    name: "Tanisha",
+    likes: ["Singing", "TT"]
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
+
+app.get("/bad", (req, res) => {
+  res.send({
+    errorMessage: "Unable to fulfil request"
+  });
+});
+
+app.listen(3000);
