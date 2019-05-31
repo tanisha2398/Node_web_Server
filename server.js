4938573e384e9ef9fe99ddca5757ae296fb9eeb8
tanisha2398 <tanishanegipro@gmail.com>
2019-05-31 20:23:25 +0530

    use handlebars to render the file in dynamic way

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 const express = require("express");
+const hbs = require("hbs");
 
 var app = express();
 
+app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   //   res.send("<h1>Hello express!</h1>");
 
-  res.send({
-    name: "Tanisha",
-    likes: ["Singing", "TT"]
+  res.render("home.hbs", {
+    pageTitle: "Home Page",
+    welcomeMsg: "Welcome to home page ",
+    currentYear: new Date().getFullYear()
   });
 });
 
 app.get("/about", (req, res) => {
-  res.send("About Page");
+  res.render("about.hbs", {
+    pageTitle: "About Page",
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get("/bad", (req, res) => {
