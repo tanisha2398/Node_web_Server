aef92d8528c7abea3974fa0b4662171073f1414f
tanisha2398 <tanishanegipro@gmail.com>
2019-05-31 21:32:21 +0530

    use handlebars helper and handlebar partial to make markup reuasble

--- server.js
+++ server.js
@@ -3,23 +3,30 @@ const hbs = require("hbs");
 
 var app = express();
 
+hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", text => {
+  return text.toUpperCase();
+});
 
 app.get("/", (req, res) => {
   //   res.send("<h1>Hello express!</h1>");
 
   res.render("home.hbs", {
     pageTitle: "Home Page",
-    welcomeMsg: "Welcome to home page ",
-    currentYear: new Date().getFullYear()
+    welcomeMsg: "Welcome to home page "
   });
 });
 
 app.get("/about", (req, res) => {
   res.render("about.hbs", {
-    pageTitle: "About Page",
-    currentYear: new Date().getFullYear()
+    pageTitle: "About Page"
   });
 });
 
